026b347fb0b58fd2134433383a2fb27abdf4e31f
PMCP <maartenlauwaert@gmail.com>
2021-01-29 18:18:07 +0000

    not center button

--- components/welcome.vue
+++ components/welcome.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="min-h-screen flex flex-col justify-center items-center">
+  <div class="min-h-screen flex flex-col justify-center items-start">
     
-    <h1 class="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-3xl pb-24">
+    <h1 class="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-3xl pb-12">
       De Nalatenschap van Kasper Demeulemeester
       Sectie Gansberg-Koekelhoren
     </h1>
@@ -9,7 +9,7 @@
     
     <button
       v-scroll-to="'#user'"
-      class=" inline-flex items-center justify-center px-6 py-3 border border-transparent text-base font-medium text-white bg-black hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500">
+      class=" inline-flex px-6 py-3 border border-transparent text-base font-medium text-white bg-black hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500">
       Welkom
     </button>
   </div>
